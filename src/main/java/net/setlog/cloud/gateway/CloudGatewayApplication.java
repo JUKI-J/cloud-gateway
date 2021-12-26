4e1b7dcf505840bdebf1da3d190ab702b6809d7e
JUKI-J <jju.ki@hotmail.com>
2021-12-26 22:14:11 +0900

    Resilience4J

--- src/main/java/net/setlog/cloud/gateway/CloudGatewayApplication.java
+++ src/main/java/net/setlog/cloud/gateway/CloudGatewayApplication.java
@@ -1,6 +1,5 @@
 package net.setlog.cloud.gateway;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
