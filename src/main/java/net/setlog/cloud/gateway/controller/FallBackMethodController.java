0c9a8a3fc7b66c85ef1efa1c19e5927ea622afeb
JUKI-J <jju.ki@hotmail.com>
2021-12-27 14:51:33 +0900

    + Configuring Resilience4J

--- src/main/java/net/setlog/cloud/gateway/controller/FallBackMethodController.java
+++ src/main/java/net/setlog/cloud/gateway/controller/FallBackMethodController.java
@@ -1,9 +1,11 @@
 package net.setlog.cloud.gateway.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/fallback")
 public class FallBackMethodController {
 
     @GetMapping("/userServiceFallBack")
